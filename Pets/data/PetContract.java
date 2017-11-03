e715c2a306c7f0b3052bbfb1320472d63c1a7e17
Lis B <eabacus@msn.com>
2017-11-03 12:11:32 -0400

    Add files via upload

--- Pets/data/PetContract.java
+++ Pets/data/PetContract.java
@@ -11,13 +11,12 @@ import android.provider.BaseColumns;
 
 public final class PetContract {
 
-    // To prevent someone from accidentally instantiating the contract class,
-    // give it an empty constructor.
+    // Use empty constructor to prevent someone from accidentally instantiating the contract class
     private PetContract() {}
 
-    /* The "Content authority" is a name for the entire content provider, similar to the
-     relationship between a domain name and its website.  A convenient string to use for the
-     content authority is the package name for the app, which is guaranteed to be unique on the
+    /* The "Content authority" is a name for the entire content provider. A convenient
+     string to use for the      content authority is the package name for the app,
+     which is guaranteed to be unique on the
      device. */
     public static final String CONTENT_AUTHORITY = "com.example.android.pets";
 
@@ -37,18 +36,18 @@ public final class PetContract {
         // The content URI to access the pet data in the provider
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
-        // The MIME type of the {@link #CONTENT_URI} for a list of pets.
+        // The MIME type of the CONTENT_URI for a list of pets.
         public static final String CONTENT_LIST_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
 
-        // The MIME type of the {@link #CONTENT_URI} for a single pet.
+        // The MIME type of the CONTENT_URI for a single pet.
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
 
         //Name of database table for pets
         public final static String TABLE_NAME = "pets";
 
-        // Unique ID number for the pet (only for use in the database table). Type: INTEGER
+        // Unique ID number for the pet used in the database table). Type: INTEGER
         public final static String _ID = BaseColumns._ID;
 
         // Name of the pet. Type: TEXT
@@ -57,19 +56,17 @@ public final class PetContract {
         // Breed of the pet. Type: TEXT
         public final static String COLUMN_PET_BREED = "breed";
 
-        // Gender of the pet. The only possible values are #GENDER_UNKNOWN, #GENDER_MALE,
-        // or #GENDER_FEMALE. Type: INTEGER
+        // Gender of the pet. 3 possible values.  Type: INTEGER
         public final static String COLUMN_PET_GENDER = "gender";
 
-        // Weight of the pet. Type: INTEGE
+        // Weight of the pet. Type: INTEGER
         public final static String COLUMN_PET_WEIGHT = "weight";
 
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
 
-        // Returns whether or not the given gender is #GENDER_UNKNOWN}, #GENDER_MALE},
-        // or #GENDER_FEMALE.
+        // Returns the gender
         public static boolean isValidGender(int gender) {
             if (gender == GENDER_UNKNOWN || gender == GENDER_MALE || gender == GENDER_FEMALE) {
                 return true;
