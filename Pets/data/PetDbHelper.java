f67257950b4ecf452e0b99b6701b11f50616e134
Lis B <eabacus@msn.com>
2017-11-03 12:12:10 -0400

    Add files via upload

--- Pets/data/PetDbHelper.java
+++ Pets/data/PetDbHelper.java
@@ -14,26 +14,18 @@ public class PetDbHelper extends SQLiteOpenHelper {
 
     public static final String LOG_TAG = PetDbHelper.class.getSimpleName();
 
-    /** Name of the database file */
+    //Name of the database file
     private static final String DATABASE_NAME = "shelter.db";
 
-    /**
-     * Database version. If you change the database schema, you must increment the database version.
-     */
+    // Database version. If you change the database schema, you must increment the database version.
     private static final int DATABASE_VERSION = 1;
 
-    /**
-     * Constructs a new instance of {@link PetDbHelper}.
-     *
-     * @param context of the app
-     */
+    // Constructs a new instance of PetDbHelper.  @param context of the app
     public PetDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    /**
-     * This is called when the database is created for the first time.
-     */
+    // This is called when the database is created for the first time.
     @Override
     public void onCreate(SQLiteDatabase db) {
         // Create a String that contains the SQL statement to create the pets table
@@ -48,9 +40,7 @@ public class PetDbHelper extends SQLiteOpenHelper {
         db.execSQL(SQL_CREATE_PETS_TABLE);
     }
 
-    /**
-     * This is called when the database needs to be upgraded.
-     */
+    // This is called when the database needs to be upgraded.
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // The database is still at version 1, so there's nothing to do be done here.
